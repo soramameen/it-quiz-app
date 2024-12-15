5df222dd842a15f7638e8cd26c47f21d5d8a7143
soramameen <sorananonaomasa.sora@gmail.com>
2024-12-15 16:34:59 +0900

    return

--- src/app/api/generateQuiz/route.ts
+++ src/app/api/generateQuiz/route.ts
@@ -7,7 +7,7 @@ import OpenAI from "openai";
 //   answer: string;
 //   explanation: string;
 // };
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const { content } = await req.json();
 
   if (!content) {
